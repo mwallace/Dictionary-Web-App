dd4ef33be5e781c234358dc0c6a72662a4a72584
Mike Wallace <mryanwallace@gmail.com>
2020-01-08 16:59:39 -0800

    Factored out Database class into its own module

---
+++ modules/database.js
@@ -0,0 +1,82 @@
+const MongoClient = require('mongodb').MongoClient;
+
+// Database handles connecting to a MongDB database and manages data in the
+// form of word:definition pairs.
+class Database {
+    constructor(url) {
+        this.url = url;
+        this.db = null;
+        this.coll = null;
+        this.isConnected = false;
+    }
+
+    async connect() {
+        this.db = await (await MongoClient.connect(this.url)).db(); 
+        this.coll = this.db.collection('test');
+        this.isConnected = true;
+    }
+
+    // Inserts a word:def pair if it word is not present in database.
+    // Updates existing definition if word is present.
+    async insertWord(word, def) {
+        if (!this.isConnected) {
+            await this.connect();
+        }
+        const doc = {
+            word: word,
+            def: def
+        };
+        let result = null;
+        try {
+            result = this.coll.updateOne(
+                { 'word': word },
+                { $set: doc },
+                { upsert: true }
+            )
+        } catch (e) {
+            console.log(e);
+        }
+        return result;
+    }
+
+    // For debug purposes: Prints out entire database
+    async printAll() {
+        if (!this.isConnected) {
+            await this.connect();
+        }
+        const cursor = await this.coll.find();
+        while( await cursor.hasNext()) {
+            const result = await cursor.next();
+            console.log('Word: ' + result.word + '; Definition: ' + result.def);
+        }
+    }
+
+    // Clears out entry based on word provided.
+    async deleteAllInstancesOf(word) {
+        if (!this.isConnected) {
+            await this.connect();
+        }
+        const query = {
+            word: word
+        }
+        const result = await this.coll.deleteMany(query);
+        console.log('Removed ' + result.deletedCount + ' occurences of entry ' + word);
+    }
+
+    // Looks up a word
+    async findWord(word) {
+        if (!this.isConnected) {
+            await this.connect();
+        }
+        const query = {
+            word: word
+        } 
+        const result = await this.coll.findOne(query);
+        if (result !== null) {
+            console.log('Found: ' + result.word + ', definition: ' + result.def);   
+        }     
+        return result;
+    }
+}
+
+module.exports = Database;
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const MongoClient = require('mongodb').MongoClient;
-
 const DATABASE_NAME = 'eng-dict';
 const MONGO_URL = 'mongodb://localhost:27017/' + DATABASE_NAME;
 
@@ -7,85 +5,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-
-// Database handles connecting to a MongDB database and manages data in the
-// form of word:definition pairs.
-class Database {
-    constructor(url) {
-        this.url = url;
-        this.db = null;
-        this.coll = null;
-        this.isConnected = false;
-    }
-
-    async connect() {
-        this.db = await (await MongoClient.connect(this.url)).db(); 
-        this.coll = this.db.collection('test');
-        this.isConnected = true;
-    }
-
-    // Inserts a word:def pair if it word is not present in database.
-    // Updates existing definition if word is present.
-    async insertWord(word, def) {
-        if (!this.isConnected) {
-            await this.connect();
-        }
-        const doc = {
-            word: word,
-            def: def
-        };
-        let result = null;
-        try {
-            result = this.coll.updateOne(
-                { 'word': word },
-                { $set: doc },
-                { upsert: true }
-            )
-        } catch (e) {
-            console.log(e);
-        }
-        return result;
-    }
-
-    // For debug purposes: Prints out entire database
-    async printAll() {
-        if (!this.isConnected) {
-            await this.connect();
-        }
-        const cursor = await this.coll.find();
-        while( await cursor.hasNext()) {
-            const result = await cursor.next();
-            console.log('Word: ' + result.word + '; Definition: ' + result.def);
-        }
-    }
-
-    // Clears out entry based on word provided.
-    async deleteWords(word) {
-        if (!this.isConnected) {
-            await this.connect();
-        }
-        const query = {
-            word: word
-        }
-        const result = await this.coll.deleteMany(query);
-        console.log('Removed ' + result.deletedCount + ' occurences of entry ' + word);
-    }
-
-    // Looks up a word
-    async findWord(word) {
-        if (!this.isConnected) {
-            await this.connect();
-        }
-        const query = {
-            word: word
-        } 
-        const result = await this.coll.findOne(query);
-        if (result !== null) {
-            console.log('Found: ' + result.word + ', definition: ' + result.def);   
-        }     
-        return result;  
-    }
-}
 
 // Path for looking up a definition, given a word
 app.get('/:word', async (req, res) => {
@@ -124,8 +43,8 @@ app.all('*', (req, res) => {
 
 // Basic stuff for getting server up and initializing db object
 const PORT = process.env.PORT || 3000;
+const Database = require('./modules/database');
 const myDb = new Database(MONGO_URL);
-const path = require('path');
 
 app.listen(PORT, function () {
     console.log('Server listening on port ' + PORT);
