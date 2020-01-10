6f4b7789b4989136a6967213982250a25f7c2e50
Mike Wallace <mryanwallace@gmail.com>
2020-01-09 17:19:58 -0800

    Added some more route tests

--- test/routes.js
+++ test/routes.js
@@ -1,5 +1,6 @@
 const app = require('../server');
 const PORT = require('../server').port;
+const URL = 'http://localhost:' + PORT;
 let expect  = require('chai').expect;
 let request = require('request');
 
@@ -8,9 +9,32 @@ describe('server', function () {
       require('../server').close();
     });
 
-    it('Main page content', function(done) {
-        request('http://localhost:' + PORT , function(error, response, body) {
+    it('Index', function(done) {
+        request(URL, function(error, response, body) {
             expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('Catch-All Route', function(done) {
+        request(URL + '/foo/bar', function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('Known Definition Lookup', function(done) {
+        request(URL + '/test', function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal(`{"_id":"5e0a8098f41fa3b28177372e","word":"test","def":"test%20definition"}`);
+            done();
+        });
+    });
+
+    it('Unknown Definition Lookup', function(done) {
+        request(URL + '/foo', function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('null');
             done();
         });
     });
